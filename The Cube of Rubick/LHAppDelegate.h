b000ff8897e912e970393c13e4082bda2c19cf8f
don1ck <vprivalov@ruswizards.com>
2013-03-18 22:12:59 +0300

    init commit

---
+++ The Cube of Rubick/LHAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  LHAppDelegate.h
+//  The Cube of Rubick
+//
+//  Created by Victor on 18.03.13.
+//  Copyright (c) 2013 LoneHouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class LHViewController;
+
+@interface LHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) LHViewController *viewController;
+
+@end
